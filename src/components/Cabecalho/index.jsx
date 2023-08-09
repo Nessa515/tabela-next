8f2411b505e24b1a15bcf287ac2195b102f23b8e
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-08-09 16:25:03 -0400

    feat: cabeçalho

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+const Cabecalho = () => {
+    return(
+        <header className={styles.cabecalho}>
+            <h3>Lista de usuários</h3>
+        </header>
+    )
+}
+
+export default Cabecalho;
---
+++ src/components/Tabela/index.jsx
@@ -0,0 +1,62 @@
+import styles from './styles.module.css';
+
+const Tabela = () => {
+    return(
+        <table className={styles.tabela}>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Ultimo login</th>
+                    <th>Tipo</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Paula Oliveira</td>
+                    <td>paula.oliveira@gmail.com</td>
+                    <td>10/03/2022 13:10</td>
+                    <td>Editor</td>
+                    <td>Ativo</td>
+                </tr>
+
+                <tr>
+                    <td>Mauro Silva</td>
+                    <td>mauro.silva@gmail.com</td>
+                    <td>11/03/2022 10:10</td>
+                    <td>Admin</td>
+                    <td>Ativo</td>
+                </tr>
+
+                <tr>
+                    <td>Júlia Freitas</td>
+                    <td>julia.freitas@gmail.com</td>
+                    <td>02/03/2022 13:00</td>
+                    <td>Editor</td>
+                    <td>Ativo</td>
+                </tr>
+
+                <tr>
+                    <td>Maria Gonçalves</td>
+                    <td>maria.goncalves@gmail.com</td>
+                    <td>08/03/2022 08:10</td>
+                    <td>Admin</td>
+                    <td>Inativo</td>
+                </tr>
+
+                <tr>
+                    <td>Paula Sakamoto</td>
+                    <td>paula.sakamoto@gmail.com</td>
+                    <td>10/03/2022 11:10</td>
+                    <td>Editor</td>
+                    <td>Ativo</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Tabela;
